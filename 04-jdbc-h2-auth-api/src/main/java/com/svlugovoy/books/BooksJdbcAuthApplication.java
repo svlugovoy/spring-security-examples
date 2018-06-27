5a679fa56e8ba1bab78c1142cf07ef5a02ba9a5f
svlugovoy <svlugovoy@gmail.com>
2018-06-27 14:47:01 +0300

    added example with jdbc auth

---
+++ 04-jdbc-h2-auth-api/src/main/java/com/svlugovoy/books/BooksJdbcAuthApplication.java
@@ -0,0 +1,12 @@
+package com.svlugovoy.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksJdbcAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksJdbcAuthApplication.class, args);
+    }
+}
